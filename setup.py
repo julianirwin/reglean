0ca1efde374259086f36275a6273ed9d2c7e918d
Julian Irwin <julian.irwin@gmail.com>
2016-08-27 16:24:41 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='reglean',
-      version='0.0.0',
+      version='0.0.1',
       description=u"Glean metadata from a filename",
       classifiers=[],
       keywords='re',
       author=u"Julian Irwin",
       author_email='julian.irwin@gmail.com',
-      url='https://bitbucket.com/gradi3nt/reglean',
+      url='https://github.com/julianirwin/reglean',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
